3629c8df51894476f6bc00eb7084e67ea2fbf325
mrtnvgr <martynovegorOF@yandex.ru>
2022-09-14 19:05:10 +0700

    smart image copy

--- cs/thsave.py
+++ cs/thsave.py
@@ -1,4 +1,4 @@
-import shutil, json, os
+import shutil, imghdr, json, os
 from cs import logger
 
 def save(cache_path, name):
@@ -13,11 +13,24 @@ def save(cache_path, name):
                 shutil.rmtree(theme_path)
             else:
                 exit(1)
-        shutil.copytree(cache_path, theme_path)
-        
+
         # Load status
+        status = json.load(open(os.path.join(cache_path, "status.json")))
+        
+        # Make theme directory
+        os.makedirs(theme_path)
+
+        # Copying files
+        cache_files = os.listdir(cache_path)
+        for file in cache_files:
+            path = os.path.join(cache_path, file)
+            if imghdr.what(path)!=None: # if file is image
+                if file!=status["wallpaper"]: # if file is not theme wallpaper
+                    os.remove(path) # cleaning old file
+                    continue
+            shutil.copy2(path, os.path.join(theme_path, file))
+        
         theme_status_path = os.path.join(theme_path, "status.json")
-        status = json.load(open(theme_status_path))
         
         # Copy wallpaper into theme directory
         wallpaper_name = os.path.basename(status["wallpaper"])
