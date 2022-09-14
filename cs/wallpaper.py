0c94c19e679e7c0666c86c5592dfe5f69362d93c
mrtnvgr <martynovegorOF@yandex.ru>
2022-09-14 07:32:35 +0700

    wallpaper mode: check if Xorg is running

--- cs/wallpaper.py
+++ cs/wallpaper.py
@@ -4,7 +4,8 @@ from cs import logger
 
 def set(path):
     if os.path.exists(path):
-        if shutil.which("feh"):
-            util.run(["feh", "--bg-fill", path])
-        else:
-            logger.warning(f"feh is not installed")
+        if os.environ.get("DISPLAY"): # xorg
+            if shutil.which("feh"):
+                util.run(["feh", "--bg-fill", path])
+            else:
+                logger.warning(f"feh is not installed")
