0135e6eb5606df8bd89e22031c2eb14b2e67d6a6
mrtnvgr <martynovegorOF@yandex.ru>
2022-10-16 19:51:09 +0700

    beautifier improvements

--- cs/util.py
+++ cs/util.py
@@ -6,23 +6,24 @@ paths["me"] = os.path.dirname(os.path.realpath(__file__))
 paths["home"] = os.getenv("HOME")
 paths["config"] = os.path.join(paths["home"], ".config", "cs")
 paths["cache"] = os.path.join(paths["home"], ".cache", "cs")
-paths["colorschemes"] = (os.path.join(paths["config"], "colorschemes"),
-                         os.path.join(paths["me"], "colorschemes"))
+paths["colorschemes"] = (
+    os.path.join(paths["config"], "colorschemes"),
+    os.path.join(paths["me"], "colorschemes"),
+)
 paths["themes"] = os.path.join(paths["config"], "themes")
 
+
 def run(cmd):
-    return subprocess.run(cmd,
-                          check=False,
-                          stderr=subprocess.DEVNULL)
+    return subprocess.run(cmd, check=False, stderr=subprocess.DEVNULL)
+
 
 def check_output(cmd):
-    return subprocess.check_output(cmd,
-                                   stderr=subprocess.DEVNULL)
+    return subprocess.check_output(cmd, stderr=subprocess.DEVNULL)
+
 
 def disown(cmd):
-    return subprocess.Popen(cmd,
-                            stdout=subprocess.DEVNULL,
-                            stderr=subprocess.DEVNULL)
+    return subprocess.Popen(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
 
 def pidof(name):
     try:
@@ -31,31 +32,39 @@ def pidof(name):
         return False
     return True
 
+
 def setupPaths():
-    """ Create path dirs if they dont exist """
+    """Create path dirs if they dont exist"""
     for folder in list(paths.values()):
-        if type(folder)==str:
+        if type(folder) == str:
             os.makedirs(folder, exist_ok=True)
         else:
             for fol in folder:
                 os.makedirs(fol, exist_ok=True)
     return paths
 
+
 def beautify(text):
-    """ Beautify text """
+    """Beautify text"""
     text = text.removesuffix(".json")
     if "_" in text:
         text = text.replace("_", " ")
     else:
         if "-" in text:
             text = text.replace("_", " ")
-    return text.title()
+
+    if len(text) > 2:
+        text = text.title()
+
+    return text
+
 
 def isUrl(text):
-    """ Check if text is url """
+    """Check if text is url"""
     return True if "http" in text and "://" in text else False
 
+
 def downloadUrl(url, path):
-    """ Download url """
+    """Download url"""
     content = requests.get(url).content
     open(path, "wb").write(content)
